5460b4b851b397d30bf967f13e2b38ba2f91cb1f
aaronsbrown <aaron.s.brown@gmail.com>
2025-03-25 21:35:42 -0700

    added velocity to note seq

--- common/modules/music/note_sequence.v
+++ common/modules/music/note_sequence.v
@@ -2,23 +2,51 @@
 module note_sequence #(
     parameter N = 3  // 3-bit index = 8 notes
 )(
-    input  wire        clk,
+    input  wire         clk,
     input  wire [N-1:0] index,
-    output reg  [7:0]  note
+    output reg  [7:0]   note,
+    output reg  [7:0]  velocity
 );
 
     // Simple synchronous ROM
     always @(posedge clk) begin
         case (index)
-            3'd0: note <= 8'd60; // C4
-            3'd1: note <= 8'd62; // D4
-            3'd2: note <= 8'd63; // Eb4
-            3'd3: note <= 8'd65; // F4
-            3'd4: note <= 8'd67; // G4
-            3'd5: note <= 8'd68; // Ab4
-            3'd6: note <= 8'd70; // Bb4
-            3'd7: note <= 8'd72; // C5
-            default: note <= 8'd0;
+            3'd0: begin
+                note <= 8'd60; // C4
+                velocity <= 8'd100;
+            end
+            3'd1:  begin
+                note <= 8'd62; // D4
+                velocity <= 8'd80;
+            end
+            3'd2:  begin
+                note <= 8'd63; // Eb4
+                velocity <= 8'd70;
+                end
+            3'd3:  begin
+                note <= 8'd65; // F4
+                velocity <= 8'd100;
+            end
+            3'd4:  begin
+                note <= 8'd67; // G4
+                velocity <= 8'd100;
+            end
+            3'd5:  begin
+                note <= 8'd68; // Ab4
+                velocity <= 8'd80;
+            end
+            3'd6:  begin
+                note <= 8'd70; // Bb4
+                velocity <= 8'd70;
+            end
+            3'd7:  begin
+                note <= 8'd72; // C5
+                velocity <= 8'd100;
+            end
+            default: begin
+                note <= 8'd0;
+                velocity <= 8'd100;
+            end
         endcase
     end
 
